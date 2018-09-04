1b6ca1116de17f5aa85630cd94c086bd9a98fa30
Alexander Nittka <alex@nittka.de>
2018-09-04 22:01:21 +0200

    #12 prepare new file wizard

---
+++ de.nittka.tooling.jtag.ui/src/de/nittka/tooling/jtag/ui/wizard/JtagFileWizard.java
@@ -0,0 +1,7 @@
+package de.nittka.tooling.jtag.ui.wizard;
+
+import org.eclipse.ui.wizards.newresource.BasicNewFileResourceWizard;
+
+public class JtagFileWizard extends BasicNewFileResourceWizard{
+
+}
