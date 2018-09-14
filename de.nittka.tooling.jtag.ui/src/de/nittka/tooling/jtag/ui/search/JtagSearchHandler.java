ff5272781ab1a56013d88406b65e5cc795ab301b
Alexander Nittka <alex@nittka.de>
2018-09-14 16:38:08 +0200

    do not execute search if there are errors

--- de.nittka.tooling.jtag.ui/src/de/nittka/tooling/jtag/ui/search/JtagSearchHandler.java
+++ de.nittka.tooling.jtag.ui/src/de/nittka/tooling/jtag/ui/search/JtagSearchHandler.java
@@ -1,5 +1,7 @@
 package de.nittka.tooling.jtag.ui.search;
 
+import java.util.List;
+
 import javax.inject.Provider;
 
 import org.eclipse.core.commands.AbstractHandler;
@@ -9,6 +11,7 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.search.ui.NewSearchUI;
 import org.eclipse.xtext.EcoreUtil2;
+import org.eclipse.xtext.diagnostics.Severity;
 import org.eclipse.xtext.nodemodel.ICompositeNode;
 import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.eclipse.xtext.resource.EObjectAtOffsetHelper;
@@ -16,7 +19,11 @@ import org.eclipse.xtext.resource.IReferenceDescription;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.editor.XtextEditor;
 import org.eclipse.xtext.ui.editor.utils.EditorUtils;
+import org.eclipse.xtext.util.CancelIndicator;
 import org.eclipse.xtext.util.concurrent.IUnitOfWork;
+import org.eclipse.xtext.validation.CheckMode;
+import org.eclipse.xtext.validation.IResourceValidator;
+import org.eclipse.xtext.validation.Issue;
 
 import com.google.common.base.Predicates;
 import com.google.inject.Inject;
@@ -29,6 +36,8 @@ public class JtagSearchHandler extends AbstractHandler {
 	private EObjectAtOffsetHelper eObjectAtOffsetHelper;
 	@Inject
 	private Provider<JtagSearchQuery> queryProvider;
+	@Inject
+	IResourceValidator validator;
 
 	//entry point adapted from FindReferencesHandler
 //	@Override
@@ -57,8 +66,27 @@ public class JtagSearchHandler extends AbstractHandler {
 		return null;
 	}
 
+	private boolean hasErrors(Search s){
+		List<Issue> errors = validator.validate(s.eResource(), CheckMode.NORMAL_AND_FAST, CancelIndicator.NullImpl);
+		if(!errors.isEmpty()){
+			String searchFragment = s.eResource().getURIFragment(s);
+			for (Issue issue : errors) {
+				if(issue.getSeverity()==Severity.ERROR){
+					String errorFragment = issue.getUriToProblem().fragment();
+					if(errorFragment.startsWith(searchFragment)){
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
 	//the essential parts from ReferenceQueryExecutor
 	public void execute(Search search) {
+		if(hasErrors(search)){
+			return;
+		}
 		JtagSearchQuery query=queryProvider.get();
 		String searchName=search.getName()!=null?search.getName():"unnamed Xarchive search";
 		//dummy initialization of ReferenceQuery
