d51790d9966618c245ee4799156eceed8ea168d0
Alexander Nittka <alex@nittka.de>
2018-10-20 10:19:21 +0200

    #46 add maximize Jtag Image View command

---
+++ de.nittka.tooling.jtag.ui/src/de/nittka/tooling/jtag/ui/view/JtagMaximizeImageViewHandler.java
@@ -0,0 +1,46 @@
+package de.nittka.tooling.jtag.ui.view;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IViewReference;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import de.nittka.tooling.jtag.ui.JtagPerspective;
+
+public class JtagMaximizeImageViewHandler extends AbstractHandler{
+
+	private IWorkbenchPart lastActivePart;
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
+		IWorkbenchPart activePart = window.getActivePage().getActivePart();
+		IWorkbenchPage[] pages = window.getPages();
+		for (IWorkbenchPage page : pages) {
+			IViewReference[] views = page.getViewReferences();
+			for (IViewReference view : views) {
+				if("de.nittka.tooling.jtag.imageView".equals(view.getId())){
+					boolean restore=view.getPage().isPageZoomed();
+					view.getPage().toggleZoom(view);
+					if(restore){
+						try{
+							if(lastActivePart!=null){
+								window.getActivePage().activate(lastActivePart);
+							}
+						}catch(Exception e){
+							JtagPerspective.logError("error while activating the previously active part", e);
+						}
+						lastActivePart=null;
+					}else{
+						lastActivePart=activePart;
+					}
+				}
+			}
+		}
+		return null;
+	}
+}
