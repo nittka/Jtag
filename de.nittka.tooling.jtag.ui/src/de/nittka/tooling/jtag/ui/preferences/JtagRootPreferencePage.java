006cb05c0f8c9325f4aa3bae6532f0243ac4de09
Alexander Nittka <alex@nittka.de>
2018-10-25 12:26:07 +0200

    add preference root page section headings

--- de.nittka.tooling.jtag.ui/src/de/nittka/tooling/jtag/ui/preferences/JtagRootPreferencePage.java
+++ de.nittka.tooling.jtag.ui/src/de/nittka/tooling/jtag/ui/preferences/JtagRootPreferencePage.java
@@ -1,6 +1,9 @@
 package de.nittka.tooling.jtag.ui.preferences;
 
 import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.xtext.ui.editor.preferences.LanguageRootPreferencePage;
 
 public class JtagRootPreferencePage extends LanguageRootPreferencePage {
@@ -12,10 +15,19 @@ public class JtagRootPreferencePage extends LanguageRootPreferencePage {
 
 	@Override
 	protected void createFieldEditors() {
+		addLabel("Jtag Editor");
 		addField(new BooleanFieldEditor(FOLD_FILE_DESCRIPTIONS, "fold file descriptions on opening Jtag editor", getFieldEditorParent()));
+		addLabel("Jtag Search");
 		addField(new BooleanFieldEditor(OPEN_HTML_BROWSER_ON_JTAG_SEARCH, "open browser for Jtag search results (Images)", getFieldEditorParent()));
 		addField(new BooleanFieldEditor(OPEN_GPS_BROWSER_ON_JTAG_SEARCH, "open browser for Jtag search results (GPS-Locations)", getFieldEditorParent()));
+		addLabel("Jtag Navigator");
 		addField(new BooleanFieldEditor(PREVENT_REFACTORING, "prevent dangerous file refactorings in Jtag Navigator", getFieldEditorParent()));
+	}
+
+	private void addLabel(String text){
+		Label label = new Label(getFieldEditorParent(), SWT.NONE);
+		label.setFont(JFaceResources.getFontRegistry().getBold(JFaceResources.DEFAULT_FONT));
+		label.setText(text);
 	}
 
 	@Override
