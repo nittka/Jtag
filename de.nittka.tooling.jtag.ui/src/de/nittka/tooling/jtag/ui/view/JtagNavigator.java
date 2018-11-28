f75d5d1b37b24a8e38c20efdeb6a44a655274075
Alexander Nittka <alex@nittka.de>
2018-11-28 11:10:55 +0100

    #49 open closed project with double click

--- de.nittka.tooling.jtag.ui/src/de/nittka/tooling/jtag/ui/view/JtagNavigator.java
+++ de.nittka.tooling.jtag.ui/src/de/nittka/tooling/jtag/ui/view/JtagNavigator.java
@@ -2,14 +2,20 @@ package de.nittka.tooling.jtag.ui.view;
 
 import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.action.StatusLineContributionItem;
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.jface.viewers.IDoubleClickListener;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.ui.navigator.CommonNavigator;
+
+import de.nittka.tooling.jtag.ui.JtagPerspective;
 
 public class JtagNavigator extends CommonNavigator {
 
@@ -20,6 +26,30 @@ public class JtagNavigator extends CommonNavigator {
 	protected void initListeners(TreeViewer viewer) {
 		super.initListeners(viewer);
 		addContainerChildCountStatusInfo(viewer);
+		addOpenClosedProjectListener(viewer);
+	}
+
+	private void addOpenClosedProjectListener(TreeViewer viewer){
+		viewer.addDoubleClickListener(new IDoubleClickListener() {
+			
+			@Override
+			public void doubleClick(DoubleClickEvent event) {
+				TreeSelection treeSelection = (TreeSelection) event.getSelection();
+				if(treeSelection.size()==1){
+					Object selection = treeSelection.getFirstElement();
+					if(selection instanceof IProject){
+						IProject project=(IProject)selection;
+						if(!project.isOpen()){
+							try {
+								project.open(new NullProgressMonitor());
+							} catch (CoreException e) {
+								JtagPerspective.logError("error while opening project", e);
+							}
+						}
+					}
+				}
+			}
+		});
 	}
 
 	private void addContainerChildCountStatusInfo(TreeViewer viewer) {
