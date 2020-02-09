5bc394653fbdafdea23086a6c90cb1025e9992d9
Alexander Nittka <alex@nittka.de>
2020-02-09 12:21:10 +0100

    update license and copyright

--- de.nittka.tooling.jtag.ui/src/de/nittka/tooling/jtag/ui/outline/IgnoreFileOutlineNode.java
+++ de.nittka.tooling.jtag.ui/src/de/nittka/tooling/jtag/ui/outline/IgnoreFileOutlineNode.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (C) 2017-2020 Alexander Nittka (alex@nittka.de)
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ ******************************************************************************/
 package de.nittka.tooling.jtag.ui.outline;
 
 import org.eclipse.emf.common.util.URI;
